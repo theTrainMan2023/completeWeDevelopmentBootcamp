f7da26d818ed3726807d4768d2a2172449ee0aff
Smiley <121711973+theTrainMan2023@users.noreply.github.com>
2023-11-08 12:32:49 +0200

    Section 16 Course Content
    Updated the Content
    Completed Section 16 - the Document Object Model

---
+++ Course Content/16.1 DOM Challenge Starting Files/index.js
@@ -0,0 +1,10 @@
+document.querySelector("li").innerHTML = "<em>Hello, Hillary</em>";
+document.querySelector("h1").style.color = "Blue";
+document.querySelector("h1").style.fontSize = "5rem"
+document.querySelector("li").style.fontSize = "2rem";
+document.querySelector(".btn").style.color = "purple";
+document.querySelector("li").style.color = "green";
+document.querySelector(".btn").style.backgroundColor = "yellow"
+document.querySelector(".btn").innerHTML = "Find me, Yoh!"
+//attributed changes
+document.querySelector("a").setAttribute("href", "http://www.bing.com");
