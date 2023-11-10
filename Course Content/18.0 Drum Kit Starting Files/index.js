fd0d438fb6a5dc2d984ae20c9eafcbcd036fc16e
Smiley <121711973+theTrainMan2023@users.noreply.github.com>
2023-11-10 16:21:37 +0200

    Completed Section 18:
    Drum challenge with Higher Functions

--- Course Content/18.0 Drum Kit Starting Files/index.js
+++ Course Content/18.0 Drum Kit Starting Files/index.js
@@ -1,4 +1,4 @@
-/** Normal or Vanilla Function toClick a Button**/
+/** Normal or Named Function toClick a Button**/
 // document.querySelector("button").addEventListener("click", handleClick);
 
 // function handleClick() {
@@ -15,6 +15,35 @@ var numberOfButtons = document.querySelectorAll(".drum").length;
 
 for(var i = 0; i < numberOfButtons; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert(" I got Clicked!");
+        // alert(" I got Clicked!");
+        /** Sound File Added**/
+        // var audio = new Audio('sounds/tom-1.mp3');
+        // audio.play();
+
+        this.style.color = "white"
     });
 }
+
+/*** CALCULATOR CODE ***/
+
+// function add(num1, num2) {
+//     return num1 + num2;
+// }
+
+// function subtract(num1, num2){
+//     return num1 - num2;
+// }
+
+// function multiply(num1, num2) {
+//     return num1 * num2;
+// }
+
+// function divide(num1, num2) {
+//     return num1 / num2;
+// }
+
+// function calculator(num1, num2, operator){
+//     return operator(num1, num2)
+// }
+
+// console.log(calculator(50, 5, divide));
