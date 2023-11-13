2d8d3ad3fec7aa16443ae6fe6abe63be6448a53a
Smiley <121711973+theTrainMan2023@users.noreply.github.com>
2023-11-13 17:40:59 +0200

    Course Content 18.0 - Drum Kit
    Completed the Website with constructor and callback functions, Methods.

--- Course Content/18.0 Drum Kit Starting Files/index.js
+++ Course Content/18.0 Drum Kit Starting Files/index.js
@@ -16,11 +16,60 @@ var numberOfButtons = document.querySelectorAll(".drum").length;
 for(var i = 0; i < numberOfButtons; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         // alert(" I got Clicked!");
-        /** Sound File Added**/
-        // var audio = new Audio('sounds/tom-1.mp3');
-        // audio.play();
+        //this method - triggers the .drum class to do something
+        // this.style.color = "white"
+        
+        // SWITCH STATMENT
+        // target the button in html
+    var buttonInnerHTML = this.innerHTML;
 
-        this.style.color = "white"
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+
+        // See line 155 - function was created
+        // switch (buttonInnerHTML) {
+        //     case "w":
+        //         /** Sound File Added**/
+        //         var tom1 = new Audio('sounds/tom-1.mp3');
+        //         tom1.play();
+        //         break;
+        //     case "a":
+        //         var tom2 = new Audio('sounds/tom-2.mp3');
+        //         tom2.play();
+        //         break;
+        //     case "s":
+        //         var tom3 = new Audio('sounds/tom-3.mp3');
+        //         tom3.play();
+        //         break;
+        //     case "d":
+        //         var tom4 = new Audio('sounds/tom-4.mp3');
+        //         tom4.play();
+        //         break;
+        //     case "j":
+        //         var crash = new Audio('sounds/crash.mp3');
+        //         crash.play();
+        //         break;
+        //     case "k":
+        //         var snare = new Audio('sounds/snare.mp3');
+        //         snare.play();
+        //         break;
+        //     case "l":
+        //         var kick = new Audio('sounds/kick-bass.mp3');
+        //         kick.play();
+        //         break;        
+        //     default:
+        //         console.log(buttonInnerHTML);
+        // }
+        //Construction Function here should look like
+        // function Audio(fileLocation) {
+        //     this.fileLocation = fileLocation;
+        //     (Method)this.play = function () {
+                //Tap into the audop hardware
+                //Check file at fileLocation
+                //check at fileLocation is sound file
+                //play the file
+        //     }
+        // }
     });
 }
 
@@ -47,3 +96,107 @@ for(var i = 0; i < numberOfButtons; i++){
 // }
 
 // console.log(calculator(50, 5, divide));
+
+// OBJECTS - Properties with Data attached to it
+//The old way of doing things
+var maidServices1 = {
+    name: "Princess",
+    age: 28,
+    experience: "true",
+    yearsOfExpereince: 15,
+    cleaningExperience: ["bedroom", "bathroom", "ballroom"],
+    langauges: ["afrikaans", "french", "english", "german"],
+    birthday: "13 May 1980"
+}
+
+// CONSTRUCTOR Fuctions - this replaces having to type out name, age etc - CREATES OBJECTS
+function BellBoy(name, age, experience, yearsOfExpereince, cleaningExperience, languages, birthday) {
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+    this.yearsOfExpereince = yearsOfExpereince;
+    this.cleaningExperience = cleaningExperience;
+    this.languages = languages;
+    this.birthday = birthday;
+    //Adding the fution into the Construction Function
+    this.moveSuitcase = function() {
+        alert("May I take your suitcase")
+        pickUpSuitcase();//another function created
+        move();//another function created
+    }
+}
+//BellBoy1.moveSuitcase();
+
+var houseKeeper1 = new HouseKeeper1("penny", 25, "true", 12, ["bedroom", hallroom], ["english", "afrikaans"], "13 May 1980")
+//console.log(houseKeeper1.name or age or experience - dot property is used);
+
+//METHODS
+//this is the OBJECT
+var maidServices1 = {
+    name: "Princess",
+    age: 28,
+    experience: "true",
+    yearsOfExpereince: 15,
+    cleaningExperience: ["bedroom", "bathroom", "ballroom"],
+    langauges: ["afrikaans", "french", "english", "german"],
+    birthday: "13 May 1980",
+    //Function associated with an OBJECT
+    moveSuitcase: function() {
+        alert("Cleaning is done")
+        cleaning();
+        
+    }
+}
+//Calling the Method
+//maidServices1.moveSuitcase();
+
+//Listening to KEYBOARD Buttons
+document.addEventListener("keydown", function (event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+    // alert("Key was pressed")
+})
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            /** Sound File Added**/
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        case "j":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case "k":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case "l":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;        
+        default:
+            console.log(buttonInnerHTML);
+    }
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey)
+    activeButton.classList.add("pressed");
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 200);
+}
