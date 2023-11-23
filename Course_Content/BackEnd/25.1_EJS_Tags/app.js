297ffbaee1d0d5bcb10e468d0f4906b18c74aece
Smiley <121711973+theTrainMan2023@users.noreply.github.com>
2023-11-23 14:25:18 +0200

    Embedded JS Completed Part 1

---
+++ Course_Content/BackEnd/25.1_EJS_Tags/app.js
@@ -0,0 +1,8 @@
+/**
+ * <%= %> - JS Output
+ * <% %> - JS Execute
+ * <%- %> - Render HTML
+ * <%% %%> - Show % or %> Escape tag
+ * <%# %> - Comemnts - Stop Execution
+ * <%- include("File Name") %> -  Insert another EJS file
+ */
